9b63daa89a207461de186ee04b3e6dcda492b2b6
KhaosKreations <35144425+KhaosKreations@users.noreply.github.com>
2018-02-15 14:49:18 -0500

    Create jquery.kk-throttle.js

---
+++ jquery.kk-throttle.js
@@ -0,0 +1,45 @@
+(function ( $ ) {
+	$.extend({
+		debounce: function(fn, delay) {
+			var timer = null;
+			return function () {
+				var context = this, args = arguments;
+				clearTimeout(timer);
+				timer = setTimeout(function () {
+					fn.apply(context, args);
+				}, delay);
+			};
+		},
+		throttle: function(func, wait, options) {
+			var context, args, result;
+			var timeout = null;
+			var previous = 0;
+			if (!options) options = {};
+			var later = function() {
+				previous = options.leading === false ? 0 : Date.now();
+				timeout = null;
+				result = func.apply(context, args);
+				if (!timeout) context = args = null;
+			};
+			return function() {
+				var now = Date.now();
+				if (!previous && options.leading === false) previous = now;
+				var remaining = wait - (now - previous);
+				context = this;
+				args = arguments;
+				if (remaining <= 0 || remaining > wait) {
+					if (timeout) {
+						clearTimeout(timeout);
+						timeout = null;
+					}
+					previous = now;
+					result = func.apply(context, args);
+					if (!timeout) context = args = null;
+				} else if (!timeout && options.trailing !== false) {
+					timeout = setTimeout(later, remaining);
+				}
+				return result;
+			};
+		}
+	});
+}( jQuery ));
